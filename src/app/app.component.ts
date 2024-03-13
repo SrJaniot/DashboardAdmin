b0c052b9a63ffafb9656bc2bf3484fe74ac3ff84
SrJaniot <estebanjaniot10@gmail.com>
2024-03-13 16:35:57 -0500

    primer push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  //Sidebar toggle show hide function
+status = false;
+addToggle()
+{
+  this.status = !this.status;
+}
 }
